ee1012ecd1442a6fd2b5f8b6f637d041e9a7c80d
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-07-05 02:52:28 +0530

    done

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,14 @@
-// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import icon from '../images/playstore.png'
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white font-bold text-xl">
-          Agri AI
+        <div className="text-white font-bold  ">
+          <img className='float-left' src={icon} alt='icon' style={{ height: '30px', width: '30px' }}></img>
+          <span className='px-2 text-xl'>  Agri AI Solutions</span>
         </div>
         <div className="space-x-4">
           <Link to="/" className="text-white hover:text-gray-200">
