8c740d2199c803c6b6b3545615f4a41ef617ccc0
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-07-07 03:26:01 +0530

    ok

--- Flask-App/app.py
+++ Flask-App/app.py
@@ -30,11 +30,11 @@ import sklearn
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Load models
-car_price_model = pickle.load(open(os.path.join(current_dir, 'models', 'car_price_model.pkl'), 'rb'))
-crop_model = pickle.load(open(os.path.join(current_dir, 'models', 'crop_recommendation_model.pkl'), 'rb'))
-fertilizer_model = pickle.load(open(os.path.join(current_dir, 'models', 'fertilizer.pkl'), 'rb'))
-classifier_model = pickle.load(open(os.path.join(current_dir, 'models', 'classifier.pkl'), 'rb'))
-soil_quality_model = pickle.load(open(os.path.join(current_dir, 'models', 'soil_quality.pkl'), 'rb'))
+# car_price_model = pickle.load(open(os.path.join(current_dir, 'models', 'car_price_model.pkl'), 'rb'))
+# crop_model = pickle.load(open(os.path.join(current_dir, 'models', 'crop_recommendation_model.pkl'), 'rb'))
+# fertilizer_model = pickle.load(open(os.path.join(current_dir, 'models', 'fertilizer.pkl'), 'rb'))
+# classifier_model = pickle.load(open(os.path.join(current_dir, 'models', 'classifier.pkl'), 'rb'))
+# soil_quality_model = pickle.load(open(os.path.join(current_dir, 'models', 'soil_quality.pkl'), 'rb'))
 
 
 
