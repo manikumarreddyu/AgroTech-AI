a8dc32dbe26f847558a900f30458a35c4a1dbe93
maridi manoj kumar <97424483+21A91A6152@users.noreply.github.com>
2024-07-08 12:03:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
- 
 export default defineConfig({
   plugins: [react({
     // Add this line
