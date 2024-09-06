70f99a18c580f8280d48d426936a3f949da3c47c
maridi manoj kumar <21a91a6152@aec.edu.in>
2024-09-06 15:08:45 +0530

    addingmap-36

--- frontend/src/MainContent.jsx
+++ frontend/src/MainContent.jsx
@@ -14,6 +14,7 @@ import Forecast from './pages/Forecast';
 import Prices from './components/Prices';
 import Reports from './components/Reports';
 import UseScrollToTop from './components/UseScrollToTop';
+import Map from './components/Map';
 
 const MainContent = () => {
   UseScrollToTop();
@@ -34,6 +35,7 @@ const MainContent = () => {
           <Route path="/forecast" element={<Forecast />} />
           <Route path="/prices" element={<Prices />} />
           <Route path="/reports" element={<Reports />} />
+          <Route path="/map" element={<Map />} />
         </Routes>
         <Footer />
       </div>
