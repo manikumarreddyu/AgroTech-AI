10dd6fdd21929231008e5e30eb3d2e68dc71e89e
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-10-06 16:08:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import MainContent from './MainContent';
-import Home from './components/Home';
-import Contact from './components/Contact';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/contact" component={Contact} />
-        {/* Add more routes here as needed */}
-      </Switch>
       <MainContent />
     </Router>
   );
