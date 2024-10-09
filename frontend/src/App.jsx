a99d401d24152011ae481321a263e9416ff34b99
Ikki <vivekprakash.st@gmail.com>
2024-10-10 01:04:57 +0530

    sugarcane-frontend-part1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MainContent from './MainContent';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <MainContent />
-    </Router>
+      </BrowserRouter>
   );
 }
 
