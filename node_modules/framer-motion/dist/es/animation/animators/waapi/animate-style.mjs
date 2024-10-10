eb4902a21d0392be9476bab2dc21ffeaa31a5383
reborn_star1 <rebornstar677@gmail.com>
2024-10-10 16:00:07 +0530

    Revamped the design of Irrigation system page

---
+++ node_modules/framer-motion/dist/es/animation/animators/waapi/animate-style.mjs
@@ -0,0 +1,12 @@
+import { GroupPlaybackControls } from '../../GroupPlaybackControls.mjs';
+import { animateElements } from './animate-elements.mjs';
+
+const createScopedWaapiAnimate = (scope) => {
+    function scopedAnimate(elementOrSelector, keyframes, options) {
+        return new GroupPlaybackControls(animateElements(elementOrSelector, keyframes, options, scope));
+    }
+    return scopedAnimate;
+};
+const animateMini = /*@__PURE__*/ createScopedWaapiAnimate();
+
+export { animateMini, createScopedWaapiAnimate };
