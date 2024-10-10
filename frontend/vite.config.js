57812ad3a0a856d727c74a629870d7ba8b06c3dd
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-10-10 16:13:18 +0530

    Revert "Revamped the design of the Irrigation Page"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src') // This points the '@/' alias to the './src' folder
-    }
-  }
 })
--- node_modules/framer-motion/dist/es/frameloop/frame.mjs
+++
@@ -1,6 +0,0 @@
-import { noop } from '../utils/noop.mjs';
-import { createRenderBatcher } from './batcher.mjs';
-
-const { schedule: frame, cancel: cancelFrame, state: frameData, steps: frameSteps, } = createRenderBatcher(typeof requestAnimationFrame !== "undefined" ? requestAnimationFrame : noop, true);
-
-export { cancelFrame, frame, frameData, frameSteps };
