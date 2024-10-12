d25b3cd35c7de9354614b935df7e6e8464ee5906
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-10-12 16:37:02 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -115,7 +115,6 @@ const Navbar = () => {
               {theme === "light" ? <FaSun className="text-2xl" /> : <FaMoon className="text-2xl" />}
             </button> */}
             
-
             {/* Authentication Links */}
             {isLoggedIn ? (
               <button onClick={logout} className="text-white hover:bg-red-500 py-2 px-3 rounded-lg transition-all duration-300">
