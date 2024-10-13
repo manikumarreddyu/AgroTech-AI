5f52297cf52110a2645b6cc1f49d598b4a949612
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-10-14 01:35:03 +0530

    done

--- frontend/src/components/PreLoader.jsx
+++ frontend/src/components/PreLoader.jsx
@@ -2,7 +2,7 @@ import "../index.css";
 //bg-gradient-to-r from-[#11cb46] via-green-600 to-[#04ba10]
 const Preloader = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex border-2 border-green-500 justify-center items-center z-50 bg-gradient-to-r from-[#01ff11] via-green-600 to-[#01ff11] ">
+    <div className="fixed top-0 left-0 w-full h-full flex border-2 border-green-500 justify-center items-center z-50 bg-gradient-to-r from-[#01ff11] via-[#01ff11] to-[#01ff11] ">
       <svg viewBox="0 0 1320 300">
         <text
           x="50%"
