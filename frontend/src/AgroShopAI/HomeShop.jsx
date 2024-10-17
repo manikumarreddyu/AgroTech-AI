f11d118d7cd6baea109c9fdfc6a505ce2897e23c
Ikki <vivekprakash.st@gmail.com>
2024-10-17 22:00:33 +0530

    added-more-categories-containers

--- frontend/src/AgroShopAI/HomeShop.jsx
+++ frontend/src/AgroShopAI/HomeShop.jsx
@@ -1,19 +1,47 @@
 import HeroSectionShop from "./components/HeroSectionShop";
-import Categories from "./components/Categories"
-import CardRelay from "./components/CardRelay"
-import {slides, categories, topSellingItems} from "./utils/home-data";
+import Categories from "./components/Categories";
+import CardRelay from "./components/CardRelay";
+import CardContainer from "./components/CardContainer";
+import LargeCardContainer from "./components/LargeCardContainer";
+import { slides, categories, topSellingItems, ongoingSale, topSellingSeeds, todaysOffers, topSellingFertilizers, organicGardeningItems } from "./utils/home-data";
 
-const HomeShop = ()=>{
-    return (
-        <div className="shop-container">
+const HomeShop = () => {
+  return (
+    <div className="shop-container bg-gray-100">
+      <HeroSectionShop images={slides} />
+      <Categories categories={categories} />
+      <img
+        src="https://github.com/IkkiOcean/AgroTech_Assets/blob/main/shop-asset/home_banner_1.jpg?raw=true"
+        alt=""
+        className="w-full mx-2 my-3" // Add this class to make the image responsive
+      />
+      <CardRelay items={topSellingItems} heading="Best Selling" />
+      <div className="flex mx-2 my-3 flex-wrap"> {/* Allow wrapping */}
+        <CardContainer items={ongoingSale} text={'Ongoing Sale'} />
+        <img
+          className=" pl-2 h-fit object-cover flex-grow" // Make the image responsive; takes half width on larger screens
+          src="https://github.com/IkkiOcean/AgroTech_Assets/blob/main/shop-asset/autumn_sale.jpg?raw=true"
+          alt="Autumn Sale"
+        />
+      </div>
+      
+      <CardRelay items={todaysOffers} heading="Today's Offer" />
+      <img
+        src="https://github.com/IkkiOcean/AgroTech_Assets/blob/main/shop-asset/home_banner_2.jpg?raw=true"
+        alt=""
+        className="w-full mx-2 my-3" // Add this class to make the image responsive
+      />
+        <CardRelay items={topSellingSeeds} heading="Top Seeds" />
+        <CardRelay items={topSellingFertilizers} heading="Top Fertilizers" />
+      <img
+        src="https://github.com/IkkiOcean/AgroTech_Assets/blob/main/shop-asset/home_banner_3.jpg?raw=true"
+        alt=""
+        className="w-full mx-2 my-3" // Add this class to make the image responsive
+      />
+      <LargeCardContainer items={organicGardeningItems} text={'Organic Farming'}/>
+      
+    </div>
+  );
+};
 
-            <HeroSectionShop  images = {slides}/>
-            <Categories categories= {categories}/>
-            <img src="https://github.com/IkkiOcean/AgroTech_Assets/blob/main/shop-asset/home_banner_1.jpg?raw=true" alt="" />
-            <CardRelay items= {topSellingItems}/>
-        </div>
-
-        
-    )
-}
 export default HomeShop;
