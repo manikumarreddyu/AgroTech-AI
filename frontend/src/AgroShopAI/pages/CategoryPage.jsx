7eabfff68dd4f4d2a8582c1ee653ccd630353379
Ikki <vivekprakash.st@gmail.com>
2024-10-19 10:35:01 +0530

    added-route

--- frontend/src/AgroShopAI/pages/CategoryPage.jsx
+++ frontend/src/AgroShopAI/pages/CategoryPage.jsx
@@ -12,7 +12,7 @@ const CategoryPage = ({ items }) => {
     <div className="flex gap-4 p-6 bg-gray-100">
       <div className="min-w-[250px] max-w-xs">
         <Filter items={items} setFilteredItems={setFilteredItems} />
-        <img className="py-2"src="/category_banner_1.jpg" alt="" />
+        <img className="py-2"src="https://github.com/IkkiOcean/AgroTech_Assets/blob/main/shop-asset/category_page/category_banner_1.jpg?raw=true" alt="" />
       </div>
       <div className="product-panel flex-grow ">
         <div className="grid grid-cols-3 gap-4 py-4 border-2 bg-green-100 rounded-lg">
