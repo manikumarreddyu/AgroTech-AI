d2706be281ad8e02873f22ad3740d72b78073022
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-10-21 15:54:09 +0530

    remmoved

--- Agro_rent/backend/constants.js
+++
@@ -1,7 +0,0 @@
-exports.constants = {
-    VALIDATION_ERROR : 400,
-    UNAUTHORIZED : 401,
-    FORBIDDEN : 403,
-    NOT_FOUND : 404,
-    SERVER_ERROR : 500
-};
--- Agro_rent/backend/routes/profileMachines.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getProfileMachines } = require('../controllers/profileMachinesController');
-const router = express.Router();
-
-router.get('/:userId', getProfileMachines); // Adjust the path as necessary
-
-module.exports = router;
