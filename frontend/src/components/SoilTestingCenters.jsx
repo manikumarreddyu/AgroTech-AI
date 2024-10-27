1b8fe84df18fc6c8bfcd3b10ec3be4cbef12a55d
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-10-27 16:40:35 +0530

    ok

--- frontend/src/components/SoilTestingCenters.jsx
+++ frontend/src/components/SoilTestingCenters.jsx
@@ -29,7 +29,7 @@ export default function SoilTestingCenters() {
     setError('');
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:5000/find_soil_labs', {
+      const response = await axios.post('https://agrotech-api.onrender.com/soil_labs', {
         location: location,
       });
 
