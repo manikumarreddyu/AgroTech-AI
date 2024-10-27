43d61fa1935699aba43dfd5980a56906cd442ab8
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-10-27 16:42:08 +0530

    Update SoilTestingCenters.jsx

--- frontend/src/components/SoilTestingCenters.jsx
+++ frontend/src/components/SoilTestingCenters.jsx
@@ -33,6 +33,7 @@ export default function SoilTestingCenters() {
         location: location,
       });
 
+      
       if (response.status === 200) {
         setLabs(response.data);
         setError('');
