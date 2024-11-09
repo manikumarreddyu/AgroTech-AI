54f8e14feced64fb1368e46d5cd1af17dfa38a62
Ikki <vivekprakash.st@gmail.com>
2024-11-09 10:38:08 +0530

    seller-components

---
+++ frontend/src/AgroShopAI/components/Pages/components/SellerAnalyticsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SellerAnalyticsPage = () => {
+  return (
+    <div>
+      <h2>Analytics</h2>
+      &quot; Add analytical tools here  &quot;
+    </div>
+  )
+}
+
+export default SellerAnalyticsPage
