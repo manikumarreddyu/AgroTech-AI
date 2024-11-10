cc16655567012701239bb4ff0f6ff68c4b1533cd
haseebzaki-07 <haseebzaki13@gmail.com>
2024-11-10 18:01:35 +0530

    add more rental routes

--- backend/routes/rent/rentOrderRoutes.js
+++ backend/routes/rent/rentOrderRoutes.js
@@ -1,12 +1,20 @@
 // routes/productRoutes.js
 
 const express = require('express');
-const { OrderConfirmation } = require('../../controllers/rent/RentOrderController');
+const { OrderConfirmation, getOrderHistory, reRent, generateReceipt } = require('../../controllers/rent/RentOrderController');
 const router = express.Router();
 
 
 // Create a new product
 router.post('/rent-product', OrderConfirmation);
+//Get product rental history
+router.get('/rent-product/history', getOrderHistory );
+// Re-rent a previously rented item
+router.post('rent-product/re-rent/:rentalId', reRent );
+// Generate detailed rental receipt
+router.get('rent-product/receipt/:rentalId', generateReceipt);
+
+
 
 
 
